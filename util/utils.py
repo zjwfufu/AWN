49dbc5c8fa0fe6db0386d3735fbb4a72b9a19457
zjwXDU <85010710+zjwXDU@users.noreply.github.com>
2023-04-07 16:49:21 +0800

    Update utils.py

--- util/utils.py
+++ util/utils.py
@@ -15,6 +15,8 @@ def fix_seed(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 
 def log_exp_settings(logger, cfg):
